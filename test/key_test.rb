4ed7bf614f8f58ce26cde867d767e2d0ad7a3783
Vince Carollo <vincecarollo@gmail.com>
2019-04-11 17:59:10 -0600

    Change generate method to create shift method to match offset class

--- test/key_test.rb
+++ test/key_test.rb
@@ -6,7 +6,7 @@ class KeyTest < Minitest::Test
   attr_reader :key
 
   def test_it_can_create_keys_5_digits_long
-    assert Key.generate.match(/^[0-9]*$/)
-    assert_equal 5, Key.generate.to_s.length
+    assert Key.create_shift.match(/^[0-9]*$/)
+    assert_equal 5, Key.create_shift.length
   end
 end
