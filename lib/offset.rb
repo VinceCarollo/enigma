9b0ff3de721e59ffc8e0efef5238ca1dba1b46b2
Vince Carollo <vincecarollo@gmail.com>
2019-04-11 19:22:57 -0600

    Set date ivar in offset for later use in return hash

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,11 +1,13 @@
 class Offset
+  attr_reader :date
+
   def self.return_shift(date)
     (date.to_i * date.to_i).to_s[-4..-1]
   end
 
   def self.create_shift
     date = Time.now.to_s[0..9].delete('-')
-    date = date[-2..-1] + date[4..5] + date[2..3]
-    (date.to_i * date.to_i).to_s[-4..-1]
+    @date = date[-2..-1] + date[4..5] + date[2..3]
+    (@date.to_i * @date.to_i).to_s[-4..-1]
   end
 end
