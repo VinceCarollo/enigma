cb5e63f11b29ae202394ac2dea2b6992be8da45c
Vince Carollo <vincecarollo@gmail.com>
2019-04-11 18:15:41 -0600

    Shorten create shift method

--- lib/offset.rb
+++ lib/offset.rb
@@ -5,10 +5,7 @@ class Offset
 
   def self.create_shift
     date = Time.now.to_s[0..9].delete('-')
-    day = date[-2..-1]
-    month = date[4..5]
-    year = date[2..3]
-    date = day + month + year
+    date = date[-2..-1] + date[4..5] + date[2..3]
     (date.to_i * date.to_i).to_s[-4..-1]
   end
 end
