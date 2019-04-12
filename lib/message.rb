5345c6e1490e218055477dc87df6889ad45ded92
Vince Carollo <vincecarollo@gmail.com>
2019-04-11 19:21:01 -0600

    Add methods to create a,b,c,d shifts for later use

--- lib/message.rb
+++ lib/message.rb
@@ -6,6 +6,22 @@ class Message
     @alphabet = ("a".."z").to_a << ' '
   end
 
+  def a_shift
+    @key_shift[0..1].to_i + @date_shift[0].to_i
+  end
+
+  def b_shift
+    @key_shift[1..2].to_i + @date_shift[1].to_i
+  end
+
+  def c_shift
+    @key_shift[2..3].to_i + @date_shift[2].to_i
+  end
+
+  def d_shift
+    @key_shift[3..4].to_i + @date_shift[3].to_i
+  end
+
   def encrypt_with_keys
     p @str
     p @key_shift
