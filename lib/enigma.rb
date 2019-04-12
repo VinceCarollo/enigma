81d7074372e5f865f43a689e4cfa1d9d3287fe8b
Vince Carollo <vincecarollo@gmail.com>
2019-04-11 20:23:41 -0600

    Set up proper ivars for hash creation

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,17 +1,19 @@
 require './lib/offset'
 require './lib/key'
+require './lib/message'
 
 class Enigma
 
   def encrypt(message, key_shift=Key.create_shift, date=nil)
+    @key_shift = key_shift
+    @date = date
     if date.nil?
       date_shift = Offset.create_shift
+      @date = Offset.create_shift.date
     else
       date_shift = Offset.return_shift(date)
     end
-    p message
-    p key_shift
-    p date_shift
+    @encryption_str = Message.new(message, key_shift, date_shift)
   end
 
 end
