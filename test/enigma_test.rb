708e7ec47df78ca76277c7b733849c6ff89536c7
Vince Carollo <vincecarollo@gmail.com>
2019-04-11 18:01:24 -0600

    Add tests for encrypt method

---
+++ test/enigma_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+  attr_reader :enigma
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, enigma
+  end
+
+  def test_it_can_encrypt
+    skip
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+  end
+end
