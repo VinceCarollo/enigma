9585073055fe87d334dcc7f5a7e72ff54352a230
Vince Carollo <vincecarollo@gmail.com>
2019-04-12 22:03:57 -0600

    Finish encrypt command line commands

--- encrypt.rb
+++ encrypt.rb
@@ -1,14 +1,14 @@
 require './lib/enigma'
 
-ARGV == ['txt_path', 'encrypt_path', 'key']
-
+ARGV == ['txt_path', 'encrypt_path']
 
 message = File.read(ARGV[0])
-enigma = Enigma.new
 message.strip!
+enigma = Enigma.new
+encrypted = enigma.encrypt(message)
 
+File.open(ARGV[1], 'w') do |file|
+  file.write encrypted[:encryption]
+end
 
-
-puts enigma.encrypt(message)[:date]
-puts enigma.encrypt(message)[:key]
-puts enigma.encrypt(message)[:encryption]
+puts "Created '#{ARGV[1]}' with key #{encrypted[:key]} and date #{encrypted[:date]}"
